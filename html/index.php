2e68a0815a20f90926feb57b7c227fdce4fd6ca2
Kacper S <77618644+Goldarian@users.noreply.github.com>
2023-06-03 11:21:25 +0200

    Add files via upload

---
+++ html/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Strona startowa PHP</title>
+</head>
+<body>
+    <h1>Strona startowa PHP</h1>
+    <?php
+    echo "Witaj na stronie startowej PHP!";
+    ?>
+</body>
+</html>
